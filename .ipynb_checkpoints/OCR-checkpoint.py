0b1c12210933d54ee1fb796c466ce8509d3f3207
Akinwande Komolafe <47945512+Sensei-akin@users.noreply.github.com>
2022-01-20 19:08:03 +0100

    Delete OCR-checkpoint.py

--- .ipynb_checkpoints/OCR-checkpoint.py
+++
@@ -1,7 +0,0 @@
-import pytesseract
-import cv2
-# Reading the image 
-image = cv2.imread('/Users/akinwande.komolafe/Downloads/Emena/letter.jpg')
-# Extraction of text from image
-text = pytesseract.image_to_string(image)
-print(text)
